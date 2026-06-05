5310be9ef10b667db8d61d18dd35a7ab48b6b5ec
ayupramudita6 <e41230760@student.polije.ac.id>
2026-06-05 10:33:49 +0700

    tambahan

---
+++ MLProject/modelling.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
+mlflow.set_experiment("Heart_Basic")
+
+df = pd.read_csv('heart_preprocessed.csv')
+X = df.drop('target', axis=1)
+y = df['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+mlflow.sklearn.autolog()
+
+with mlflow.start_run(run_name="RandomForest_Basic"):
+    model = RandomForestClassifier(n_estimators=100, random_state=42)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+    print(f"Akurasi: {acc:.4f}")
